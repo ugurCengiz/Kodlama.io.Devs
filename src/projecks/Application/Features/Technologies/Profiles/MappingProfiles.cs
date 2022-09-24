ee2a423890fdb54e31d96b47ddf53c2dbd61aab5
uğur cengiz <ugur_nobre@hotmail.com>
2022-09-24 16:04:19 +0300

    24.09.22

--- src/projecks/Application/Features/Technologies/Profiles/MappingProfiles.cs
+++ src/projecks/Application/Features/Technologies/Profiles/MappingProfiles.cs
@@ -7,6 +7,7 @@ using Application.Features.ProgrammingLanguages.Commands.CreateProgrammingLangua
 using Application.Features.ProgrammingLanguages.Dtos;
 using Application.Features.Technologies.Commands.CreateTechnology;
 using Application.Features.Technologies.Commands.DeleteTechnology;
+using Application.Features.Technologies.Commands.UpdateTechnology;
 using Application.Features.Technologies.Dtos;
 using Application.Features.Technologies.Models;
 using Application.Services.Repositories;
@@ -31,6 +32,9 @@ namespace Application.Features.Technologies.Profiles
 
             CreateMap<Technology, DeletedTechnologyDto>().ReverseMap();
             CreateMap<Technology, DeleteTechnologyCommand>().ReverseMap();
+
+            CreateMap<Technology, UpdatedTechnologyDto>().ReverseMap();
+            CreateMap<Technology, UpdateTechnologyCommand>().ReverseMap();
 
         }
     }
